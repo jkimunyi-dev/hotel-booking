c3f6bbf5d2fef57e8fb61df48a09973937feffca
jimmie <jkkimunyi@gmail.com>
2024-10-21 15:05:50 +0300

    Added customd db configuration

---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php
+    try{
+        //host
+        define("HOST", "localhost");
+
+        // dbname
+        define("DBNAME", "hotel-bookings");
+
+        // user
+        define("USER","root");
+
+        // password
+        define("PASS","");
+
+        $conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME."", USER, PASS);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // if($conn == true) {
+        //     echo "Connected to DB";
+        // }else{
+        //     echo "Failed to connect to DB";
+        // }
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
